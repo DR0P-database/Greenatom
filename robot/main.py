2577d06ff02f209e9c8b9c3bbea6fc1a194bdbe9
Алексей Уханов <dropdatabase.py@gmail.com>
2024-04-06 00:48:02 +0300

    Add robot/main.py
    This file is robot that start counter, run only with int parametr

---
+++ robot/main.py
@@ -0,0 +1,19 @@
+import sys
+import asyncio
+
+
+async def run_robot(n: int) -> None:
+    while True:
+        print(n)
+        n += 1
+        await asyncio.sleep(1)
+
+
+async def main():
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(run_robot(int(sys.argv[1])))
+
+
+if __name__ == "__main__":
+    # print('[INFO] Robot started from', sys.argv[1])
+    asyncio.run(main())
