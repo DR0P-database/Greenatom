2d1911095a5456b50c558f453a4c15a0b77338ed
Алексей Уханов <dropdatabase.py@gmail.com>
2024-04-06 01:54:02 +0300

    Alot feature
    - Add func to find pid process by name
    - Add func to kill process by pid
    - All robots run in multiprocessing
    - Stop robot work with pid

--- server/app.py
+++ server/app.py
@@ -3,11 +3,30 @@ import platform
 import subprocess
 import uvicorn
 import os
+import psutil
 import multiprocessing
 
 app = FastAPI()
 
-robot_process = None
+
+def find_pid_by_script_name(script_name):
+    for proc in psutil.process_iter():
+        try:
+            if ("Python" in proc.name() or "python" in proc.name()) and script_name in proc.cmdline():
+                return proc.pid
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return None
+
+
+def kill_process_by_pid(pid):
+    try:
+        if platform.system() == 'Windows':
+            os.system(f"taskkill /F /PID {pid}")
+        elif platform.system() == 'Darwin':
+            os.kill(pid, 9)
+    except Exception as e:
+        print(f"Failed to terminate process with PID {pid}: {e}")
 
 
 def get_absolute_path(file_name):
@@ -17,31 +36,34 @@ def get_absolute_path(file_name):
     return absolute_path
 
 
-def run_robot_process(start_number):
+def run_subprocess(start_number):
     if platform.system() == 'Windows':
-        os.system(' '.join(['start cmd /c', 'python',
-                  get_absolute_path("robot/main.py"), start_number]))
+        subprocess.run(['start cmd /c', 'python',
+                        get_absolute_path("robot/main.py"), str(start_number)])
     elif platform.system() == 'Darwin':
-        os.system(
-            ' '.join(['python', get_absolute_path("robot/main.py"), start_number]))
+        subprocess.run(['python', get_absolute_path(
+            "robot/main.py"), str(start_number)])
 
 
 @app.post('/start_robot/{start_number}')
 @app.post('/start_robot')
 async def start_robot(start_number: int = 0) -> dict:
-    global robot_process
-    p = multiprocessing.Process(
-        target=run_robot_process, args=(str(start_number), ))
-    robot_process = p
-    p.start()
+
+    multiprocessing.Process(target=run_subprocess,
+                            args=(start_number, )).start()
     return {'message': 'Robot started'}
 
 
 @app.post('/stop_robot')
 async def stop_robot():
-    global robot_process
-    robot_process.terminate()
-    return {'message': 'Robot stopped'}
+    script_name = get_absolute_path("robot/main.py")
+    pid = find_pid_by_script_name(script_name)
+    if pid:
+        kill_process_by_pid(pid)
+        return {'message': 'Robot stopped'}
+
+    return {'message': 'Robot NOT found'}
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
